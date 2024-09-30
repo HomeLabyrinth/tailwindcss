20548e5db70b9f1e697c692e0e22c40186dba139
HomeLabyrinth <HomelabyrinthManager@gmail.com>
2024-09-30 13:22:15 -0500

    Create tailwind.config.js
    creation of the tailwind import file

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+// tailwind.config.js
+const colors = require('tailwindcss/colors')
+
+module.exports = {
+  theme: {
+    colors: {
+      // Build your palette here
+      transparent: 'transparent',
+      current: 'currentColor',
+      gray: colors.trueGray,
+      red: colors.red,
+      blue: colors.sky,
+      yellow: colors.amber,
+    }
+  }
+}
